d0c98fd9b79827fc2829cddcb19dceb14ff980d9
Jean-Christophe Gay <jean-christophe.gay@vidal.fr>
2024-05-15 07:18:46 +0200

    Make JAXBContext static to improve performance
    A XmlAdapter is not a singleton in JAXB lifecyle, instantiating
    a JAXBContext in each ExtensionElementAdapter is costly 😇.
    The class is threadsafe and MUST be reuse: https://stackoverflow.com/questions/7400422/jaxb-creating-context-and-marshallers-cost
    
    In our API in production, this cost us around 6% of response time based on flame graph analysis 😅.
    
    Benchmark                        Mode  Cnt     Score    Error  Units
    Marshaller.testMarshallingFeed  thrpt   25  5501,647 ± 61,776  ops/s
    
    Fixes #115

--- src/main/java/fr/vidal/oss/jaxb/atom/core/AtomExtensionException.java
+++ src/main/java/fr/vidal/oss/jaxb/atom/core/AtomExtensionException.java
@@ -1,6 +1,6 @@
 package fr.vidal.oss.jaxb.atom.core;
 
-public class AtomExtensionException extends Exception {
+public class AtomExtensionException extends RuntimeException {
 
     AtomExtensionException(String message, Throwable cause) {
         super(message, cause);
